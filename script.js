a42560a3e478587709c3ed391746356fe7512415
Gustavo Miranda <guspnet@gmail.com>
2022-12-07 18:42:31 -0300

    Aula 01 do Curso JavaScript: Consumindo e tratando dados de uma API

---
+++ script.js
@@ -0,0 +1,12 @@
+console.log("Mandando oi pro amigo!");
+
+function mandaMensagem() {
+    console.log("Tudo bem?");
+    console.log("Vou te mandar solicitação!");
+    console.log("Solicitação recebida!");
+}
+
+setTimeout(mandaMensagem, 5000);
+
+
+console.log("Tchau tchau!");
